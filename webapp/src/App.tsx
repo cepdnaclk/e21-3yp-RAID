629acc2e7915bc52bf3d88a0e0f8c3f245756182
suvini2001 <e21140@eng.pdn.ac.lk>
2026-04-18 16:47:00 +0530

    web app front end update

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
         <AlertProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
