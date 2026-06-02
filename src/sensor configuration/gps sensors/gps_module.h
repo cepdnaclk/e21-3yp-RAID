a8647f96e298f54e770f5757f9983557e7d3e2bb
Tharushi Savindi <e21363@eng.pdn.ac.lk>
2026-06-02 16:36:56 +0530

    Add AS5600 encoder odometry and GPS functionality

--- src/sensor configuration/gps sensors/gps_module.h
+++ src/sensor configuration/gps sensors/gps_module.h
@@ -6,8 +6,8 @@
 #include <HardwareSerial.h>
 
 // Define the ESP32 Hardware UART pins for the NEO-6M
-#define GPS_RX_PIN 16
-#define GPS_TX_PIN 17
+#define GPS_RX_PIN 18
+#define GPS_TX_PIN 9
 #define GPS_BAUD_RATE 9600
 
 class GPSModule {
